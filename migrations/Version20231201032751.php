a99d10825617ad4a3090b5eb663d59259c947f49
Jaison Mora <jasonjairmoramora@gmail.com>
2023-11-30 22:59:58 -0500

    inicio de session y registro de usuario

--- migrations/Version20231201032751.php
+++ migrations/Version20231201032751.php
@@ -19,7 +19,7 @@ final class Version20231201032751 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET latin1 COLLATE `latin1_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET latin1 COLLATE `latin1_spanish_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
